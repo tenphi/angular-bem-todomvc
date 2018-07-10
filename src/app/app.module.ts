1989122ee01b8c8ca21b9b7cd7d6d36a9f6d4ac6
Andrey Yamanov <tenphi@gmail.com>
2018-07-10 19:57:48 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BemModule } from 'angular-bem';
+
+import { TodoApp }  from './app.component';
+import { TodoStore } from './services/store';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, BemModule ],
+  declarations: [ TodoApp ],
+  providers:    [ TodoStore ],
+  bootstrap:    [ TodoApp ]
+})
+export class AppModule { }
